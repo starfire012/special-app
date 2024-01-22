9ddbfe022f0b5e36545cbbb2cf8f9e883815fa90
skillbox <starfire3596@gmail.com>
2024-01-22 17:31:28 +0300

    g-h-j

--- main.py
+++ main.py
@@ -12,7 +12,7 @@ def print_hi(name):
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
     print_hi('Star!')
-    print ('good job!')
+    print ('good jo!')
 
 
 
