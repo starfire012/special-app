1eb4d73e29d959b38c8c37705c678b219cc848b7
starfire012 <157134299+starfire012@users.noreply.github.com>
2024-01-22 10:57:46 +0300

    sixth remote

--- main.py
+++ main.py
@@ -12,6 +12,6 @@ def print_hi(name):
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
     print_hi('El')
-    print('good job')
+    print('good job!')
 
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
