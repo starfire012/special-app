bcf02c0ca380f78b5886e73bfb722bb6eeaf617c
skillbox <starfire3596@gmail.com>
2024-01-22 10:58:18 +0300

    sixth local

--- main.py
+++ main.py
@@ -11,6 +11,7 @@ def print_hi(name):
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    print_hi('El')
+    print_hi('Star')
+
 
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
